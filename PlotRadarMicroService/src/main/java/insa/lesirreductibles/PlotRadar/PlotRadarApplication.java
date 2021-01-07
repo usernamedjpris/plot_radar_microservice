c3a23195374c07c3559ee6ae447f62e79c3e816a
usernamedjpris <jeremie.gantet@gmail.com>
2021-01-07 17:10:14 +0100

    maven project added

---
+++ PlotRadarMicroService/src/main/java/insa/lesirreductibles/PlotRadar/PlotRadarApplication.java
@@ -0,0 +1,13 @@
+package insa.lesirreductibles.PlotRadar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlotRadarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlotRadarApplication.class, args);
+	}
+
+}
---
+++ PlotRadarMicroService/src/test/java/insa/lesirreductibles/PlotRadar/PlotRadarApplicationTests.java
@@ -0,0 +1,13 @@
+package insa.lesirreductibles.PlotRadar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlotRadarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
