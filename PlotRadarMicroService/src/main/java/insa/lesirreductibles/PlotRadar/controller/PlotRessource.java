3a8589123292d78388138bb25cd59e09f9908b16
usernamedjpris <jeremie.gantet@gmail.com>
2021-01-12 22:06:59 +0100

    microservice tested

--- PlotRadarMicroService/src/main/java/insa/lesirreductibles/PlotRadar/controller/PlotRessource.java
+++ PlotRadarMicroService/src/main/java/insa/lesirreductibles/PlotRadar/controller/PlotRessource.java
@@ -1,3 +1,4 @@
+
 package insa.lesirreductibles.PlotRadar.controller;
 
 import java.io.BufferedReader;
@@ -11,13 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/graph") 
+
+
 public class PlotRessource {
 	
-	@GetMapping("/{aaai}") 
-	public String getTrajectory(@PathVariable("aaai") String id) throws IOException, InterruptedException{ 
-		
+	@GetMapping("") 
+	public String getInfos() { 
+		return "<h1>Ploting Radar Graph</h1><div>localhost:8080/graph/YY-MM-DD-HH:mm_YY-MM-DD-HH:mm/AI/AA</div><div><a href=\"localhost:8080/graph/20-07-29-10:00_20-07-29-12:00/EVX02EK/38173A\">localhost:8080/graph/20-07-29-10:00_20-07-29-12:00/EVX02EK/38173A</div>";
+        } 
+	@GetMapping("/{dates}/{ai}/{aa}") 
+	public String getTrajectory(@PathVariable("dates") String dates,@PathVariable("ai") String ai,@PathVariable("aa") String aa) throws IOException, InterruptedException{ 
+	dates = dates.replace("_"," ");
 	//String command = "cmd.exe /c E:/eDocuments/\"projet intégrateur\"/plot_radar_microservice/scripts/plotRadar2.py 01:00:5e:50:00:26 12-12-2020 4841AA TRA39U"; 
-	        String command = "python3 /home/user/Documents/plot_radar_microservice/scripts/plotRadar.py 01:00:5e:50:00:26 20-07-29-10:00 20-07-29-12:00 EVX02EK 38173A" 	;
+	        String command = "python3 /home/user/Documents/plot_radar_microservice/scripts/plotRadar.py 25 "+dates+" "+ai+" "+aa; //01:00:5e:50:00:26 
                 Process p = Runtime.getRuntime().exec(command); 
      	p.waitFor();  
      	String pathResult = "";
@@ -29,6 +36,6 @@ public class PlotRessource {
             }                                                                                                                    
         } 
 
-		return pathResult; 
+		return "<div><b>Dates</b> : "+dates+"</div><div><b>AI</b> : "+ai+"</div><div><b>AA</b> : "+aa+"</div><div><b>Image enregistree</b> : "+pathResult+"</div>"; 
 	} 
 }
--- scripts/plotRadar.py
+++ scripts/plotRadar.py
@@ -39,8 +39,8 @@ def angle2xy(theta,rho):
 
 
 def format_date(date):
-    date = date[0:7]+date[9:]
-    return date.replace("-","/")+"-00:00"
+    date = date[:2]+"/"+date[3:5]+"/"+date[6:]
+    return date
 
 
 
@@ -48,8 +48,8 @@ def read_data(datedebut,datefin):
     """read data from a csv file (soon : from a database)"""
     #os.chdir("E:/eDocuments/projet intégrateur/asterix_big_data/parser_tests/")
     #df = pd.read_csv('../../data/new_data.csv', delimiter=';')
-    #df = dataaccess.query_range_date_df(format_date(datedebut),format_date(datefin))
-    df = dataaccess.query_range_date_df("20/07/29-10:00","20/07/29-12:00") #("29/07/20-10:00","29/07/20-12:00")
+    df = dataaccess.query_range_date_df(format_date(datedebut),format_date(datefin))
+    #df = dataaccess.query_range_date_df("20/07/29-10:00","20/07/29-12:00") #("29/07/20-10:00","29/07/20-12:00")
     return df
     
 
@@ -64,14 +64,13 @@ def plot_flight(df,identification,datedebut,datefin):
     for i in range(df.shape[0]):
         plt.plot(df.iloc[i]["X"],df.iloc[i]["Y"],color='#444')
     df = df.set_index('ID')
-    
+     #print(df.head())
     
     # ploting the desired flight
     flight = df.loc[identification]     
     plt.plot(flight["X"],flight["Y"],"-",color="#FF7A86", linewidth=3)
     ax.add_artist(plt.Circle((flight["X"][-1],flight["Y"][-1] ), 1.5, color='#FFBAC6', linewidth=1,zorder=10))
     ax.add_artist(plt.Circle((flight["X"][-1],flight["Y"][-1] ), 3, color='#FFBAC6', fill=None, linewidth=1,zorder=10))
-    
     
     # adding some cool style 
     for spine in ['bottom','top','right','left']:
@@ -84,9 +83,9 @@ def plot_flight(df,identification,datedebut,datefin):
     plt.title("AI: "+identification.split(" ")[0]+"    AA:"+identification.split(" ")[-1])  
     
     # saving file and returning PATH
-    RELATIVE_PATH = "/plots/"+identification+" "+datedebut+" - "+datefin+".png"    
+    RELATIVE_PATH = "/plots/"+identification.replace(" ","_")+"_"+datedebut.replace(":","-")+"_"+datefin.replace(":","-")+".png"    
     plt.savefig("."+RELATIVE_PATH)
-    print("\n",os.getcwd()+RELATIVE_PATH)
+    print(os.getcwd()+RELATIVE_PATH)
     #plt.show()
     
 
@@ -95,17 +94,17 @@ def plot_flight(df,identification,datedebut,datefin):
 
 def main(argv):
     """plot radar and return path of the image"""
-  
+
     # Deal with args
     if len(sys.argv) == 5:
-        radar, datedebut, identification = sys.argv[1], sys.argv[2],str(sys.argv[3])+" "+str(sys.argv[4])
+        radar, datedebut, identification = int(sys.argv[1]), sys.argv[2],str(sys.argv[3])+" "+str(sys.argv[4])
         datefin = datedebut[:-5]+"23:59" #a changer : il faut mettre jusqu'a la fin de la journee
     elif len(sys.argv) == 6:
-        radar, datedebut,datefin, identification = sys.argv[1], sys.argv[2], sys.argv[3], str(sys.argv[4])+" "+str(sys.argv[5])
+        radar, datedebut,datefin, identification = int(sys.argv[1]), sys.argv[2], sys.argv[3], str(sys.argv[4])+" "+str(sys.argv[5])
         #radar, datedebut,datefin, identification = sys.argv[1], sys.argv[2], sys.argv[3], str(sys.argv[4])
 
     else:
-        print('usage: python plotRadar.py @radar YY-MM-DD-HH:mm YY-MM-DD-HH:mm AI AA\nusage: python3 plotRadar.py @radar YY-MM-DD-HH:mm AI AA\nexample: python plotRadar.py 01:00:5e:50:00:26 20-07-29-10:00 20-07-29-12:00 EVX02EK 38173A')
+        print('usage: python plotRadar.py @radar YY-MM-DD-HH:mm YY-MM-DD-HH:mm AI AA\nusage: python3 plotRadar.py @radar YY-MM-DD-HH:mm AI AA\nexample: python plotRadar.py 25 20-07-29-10:00 20-07-29-12:00 EVX02EK 38173A')
         sys.exit(12)
         #datedebut, datefin, identification= "12-12-2020", "12-12-2020", "TRA39U 4841AA"#"FPO6610 39666F"VS"JAF7FE 44A835" #"VLG8191 343194"(landed)#"TAR724 02A194"#"AFR1390 3991E1" #"AAF525 398005"
         
@@ -117,13 +116,28 @@ def main(argv):
     # ID
     df2["ID"] = [str(tid).replace(" ","")+" "+str(aa).replace(" ","") for tid, aa in zip(df2["tid"],df2["aa"])]
     #df2["ID"] = df2["aa"]
+    
+    try:
+         df2 = df2[df2["sic"]==25]
+         print("sic =",25)
+         print(df2.shape)
+    except:
+         df2 = df2[df2["sic"]==df2.iloc[1]["sic"]]
+         print("sic[1] =",df2.iloc[1]["sic"])
+         print(df2.iloc[1])
+    
+    
     # X, Y
     df2["X"],df2["Y"]=angle2xy(convert(df2["theta"]),df2["rho"])
-        
+    
     # Aggregation on ID
     df3 = df2.groupby('ID')['X'].apply(list).reset_index(name='X')
     df3['Y'] = df2.groupby('ID')['Y'].apply(list).reset_index(name='Y')['Y']
     
+    #print(df2.iloc[1])
+    #print(df3.iloc[1])
+    print(df3.shape)
+
     # Plot radar
     try:
         plot_flight(df3,identification,datedebut,datefin)
